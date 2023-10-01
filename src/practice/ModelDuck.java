2c90be53bc5ddc9d803923b7fc57eeff76afad08
MUHAMAD REIKHAN EFENDI <muhamadreikhan69@gmail.com>
2023-10-01 15:25:26 +0700

    first commit

---
+++ src/practice/ModelDuck.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practice;
+
+/**
+ *
+ * @author REIKHAN
+ */
+public class ModelDuck extends Duck{
+    
+    public ModelDuck(){
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+    
+    @Override
+    public void display(){
+        System.out.println("I'm a model duck");
+    }
+    
+}
